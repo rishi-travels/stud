fc4be192750bb07e5bbb4243d0ab286bae4a3c24
Intel Core <intelcorei5136@gmail.com>
2026-03-21 15:12:28 +0000

    https://cdn.bajajauto.com/-/media/assets/bajajauto/bikes/pulsar-k-2024/h

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's1.cdn.autoevolution.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
