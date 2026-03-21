b9fa4fff000f95d838f00c094a9cac96c37f42d4
Intel Core <intelcorei5136@gmail.com>
2026-03-21 15:29:10 +0000

    https://s1.cdn.autoevolution.com/images/moto_gallery/BAJAJ-MOTORS-Pulsar

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -67,6 +68,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'gaadiwaadi.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.bikes4sale.in',
         port: '',
         pathname: '/**',
       },
