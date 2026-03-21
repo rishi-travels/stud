40f5ae544b705b6cde8bbf0bc1f7fd54f51da3bf
Intel Core <intelcorei5136@gmail.com>
2026-03-21 16:22:57 +0000

    in comfort collection change the img of chetak urbane with https://media

--- next.config.ts
+++ next.config.ts
@@ -131,6 +131,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.zigcdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ const PERFORMANCE_VEHICLES = [
 
 const ECO_VEHICLES = [
   { name: "Chetak Premium", type: "Electric Scooter", image: PlaceHolderImages.find(p => p.id === "chetak-electric") },
-  { name: "Chetak Urbane", type: "Smart EV", image: PlaceHolderImages.find(p => p.id === "chetak-urbane") },
+  { name: "Platina 110", type: "Comfort Commuter", image: PlaceHolderImages.find(p => p.id === "platina-110") },
   { name: "Freedom 125", type: "Innovative", image: PlaceHolderImages.find(p => p.id === "freedom-125") },
 ];
 
