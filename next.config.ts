dd70e27fcaffdc157edad56bae178e4fca45134c
Intel Core <intelcorei5136@gmail.com>
2026-03-21 16:18:10 +0000

    in comfort collection change the img of chetak premium  with https://www

--- next.config.ts
+++ next.config.ts
@@ -125,6 +125,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.bikeleague.in',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
