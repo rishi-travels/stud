d90c05cf4aa138d5f7834dd1d60d09f265d4bab2
Intel Core <intelcorei5136@gmail.com>
2026-03-21 13:04:08 +0000

    https://english.onlinekhabar.com/wp-content/uploads/2023/07/Screenshot-2

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'english.onlinekhabar.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
