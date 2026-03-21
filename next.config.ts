4a8866f5a07ea8084476030f19b5c837c17be722
Intel Core <intelcorei5136@gmail.com>
2026-03-21 15:55:50 +0000

    change image with https://somosmoto.pe/images/models/gallery/bajaj-pulsa

--- next.config.ts
+++ next.config.ts
@@ -107,6 +107,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'somosmoto.pe',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
