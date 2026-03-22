4828adca7fde2bd563b45713322dc6920c0f48f0
Intel Core <intelcorei5136@gmail.com>
2026-03-22 17:32:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7948651386-d2ebf",
+  "appId": "1:750766554759:web:96ed0ca1bd23bebe62c222",
+  "apiKey": "AIzaSyDByZUwFtFurWERphVwEEVnLDeSZ5pdmxI",
+  "authDomain": "studio-7948651386-d2ebf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "750766554759"
+};
